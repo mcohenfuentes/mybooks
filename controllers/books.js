4fc3b13f344433148c3e0a40b58f3bf1598ad33c
Mica CF <mcohenfuentes@gmail.com>
2025-02-24 16:57:08 -0500

    added new page, data to index page, show page, delete funct ionality, edit page, edit functionality

--- controllers/books.js
+++ controllers/books.js
@@ -5,11 +5,107 @@ const User = require('../models/user.js');
 
 router.get('/', async (req, res) => {
     try {
-      res.render('books/index.ejs');
+
+      const currentUser = await User.findById(req.session.user._id);
+      
+      res.render('books/index.ejs', {
+        books: currentUser.books,
+      });
+    } catch (error) {
+     
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
+  router.get('/new', async (req, res) => {
+    res.render('books/new.ejs');
+  });
+
+  router.post('/', async (req, res) => {
+    try {
+  
+      const currentUser = await User.findById(req.session.user._id);
+     
+      currentUser.books.push(req.body);
+   
+      await currentUser.save();
+     
+      res.redirect(`/users/${currentUser._id}/books`);
+    } catch (error) {
+     
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
+  router.get('/:bookId', async (req, res) => {
+    try {
+  
+      const currentUser = await User.findById(req.session.user._id);
+      console.log(currentUser)
+      const book = currentUser.books.id(req.params.bookId);
+      console.log(book)
+      
+      
+      res.render('books/show.ejs', {
+        book: book,
+      });
+    } catch (error) {
+      
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
+  router.delete('/:bookId', async (req, res) => {
+    try {
+     
+      const currentUser = await User.findById(req.session.user._id);
+     
+      currentUser.books.id(req.params.bookId).deleteOne();
+ 
+      await currentUser.save();
+    
+      res.redirect(`/users/${currentUser._id}/books`);
+    } catch (error) {
+   
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
+  router.get('/:bookId/edit', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      const book = currentUser.books.id(req.params.bookId);
+      res.render('books/edit.ejs', {
+        book: book,
+      });
     } catch (error) {
       console.log(error);
       res.redirect('/');
     }
   });
-  
+
+  router.put('/:bookId', async (req, res) => {
+    try {
+     
+      const currentUser = await User.findById(req.session.user._id);
+      
+      const book = currentUser.books.id(req.params.bookId);
+      
+      book.set(req.body);
+     
+      await currentUser.save();
+      
+      res.redirect(
+        `/users/${currentUser._id}/books/${req.params.bookId}`
+      );
+    } catch (error) {
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
 module.exports = router;
